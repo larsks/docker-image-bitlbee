40226a2cd767f11c972b9a929aa42d29d561d05e
Lars Kellogg-Stedman <lars@redhat.com>
2015-09-30 14:33:51 -0400

    ensure ownership on configdir is appropriate

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 CFDIR=/etc/bitlbee
 
 : ${BITLBEE_OPERPASSWORD:=$(dd if=/dev/urandom bs=1 count=20 | base64)}
+: ${BITLBEE_USER:=bitlbee}
+: ${BITLBEE_GROUP:=bitlbee}
 : ${BITLBEE_HOSTNAME:=bitlbee}
 : ${BITLBEE_DAEMONPORT:=6667}
 : ${BITLBEE_CONFIGDIR:=/var/lib/bitlbee}
@@ -11,6 +13,7 @@ if [ -f "$CFDIR/bitlbee.conf.in" ] && ! [ -f "$CFDIR/bitlbee.conf" ] ; then
 sed '
 s|\$OPERPASSWORD|'"$BITLBEE_OPERPASSWORD"'|
 s|\$HOSTNAME|'"$BITLBEE_HOSTNAME"'|
+s|\$USER|'"$BITLBEE_USER"'|
 s|\$DAEMONPORT|'"$BITLBEE_DAEMONPORT"'|
 s|\$CONFIGDIR|'"$BITLBEE_CONFIGDIR"'|
 ' < "$CFDIR/bitlbee.conf.in" > "$CFDIR/bitlbee.conf"
@@ -27,5 +30,7 @@ Operator password: $BITLBEE_OPERPASSWORD
 ======================================================================
 
 EOF
+
+chown -R ${BITLBEE_USER}:${BITLBEE_GROUP} ${BITLBEE_CONFIGDIR}
 
 exec /usr/sbin/bitlbee -nv
